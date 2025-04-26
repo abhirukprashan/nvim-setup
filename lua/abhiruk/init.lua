4d06763ac7603d2a96e67d12f5f2642e0ccaff8b
abhiruk <abhirukprashan436@gmail.com>
2025-04-26 18:48:49 +0530

    op

--- lua/abhiruk/init.lua
+++ lua/abhiruk/init.lua
@@ -10,9 +10,5 @@ require("one_monokai").setup({
     italics = true,
 })
 vim.cmd("colorscheme one_monokai")
--- vim.cmd("colorscheme rose-pine-main")
--- vim.cmd("colorscheme rose-pine-moon")
--- vim.cmd("colorscheme rose-pine-dawn")
---
 require("telescope").load_extension('harpoon')
 print("hello from abhiruk")
